466d25936f980ff54e28193904c5ccdd7dbb69ae
nexisn4 <nexisn4@gmail.com>
2023-11-16 09:51:14 -0600

    format

--- js/age.js
+++ js/age.js
@@ -1,15 +1,18 @@
-var dob = new Date("08/17/1995");
-//calculate month difference from current date in time
-var month_diff = Date.now() - dob.getTime();
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    var dob = new Date("08/17/1995");
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
 
-//convert the calculated difference in date format
-var age_dt = new Date(month_diff); 
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff); 
 
-//extract year from date    
-var year = age_dt.getUTCFullYear();
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
 
-//now calculate the age of the user
-var age = Math.abs(year - 1970);
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
 
-//display the calculated age
-document.write("Ue is " + age + " years");
+    //display the calculated age
+    $('#age').text("Ue is " + age + " years");
+});
